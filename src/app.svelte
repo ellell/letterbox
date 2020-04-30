eb2edb6b6da4bf957a3086f1247dbcb30f5b8ca1
lisa-overmyr <lisa.overmyr@schibsted.com>
2020-04-30 19:15:30 +0200

    styling updates

--- src/app.svelte
+++ src/app.svelte
@@ -82,11 +82,29 @@
     }
   }
 
-  .letter-box {
-    width: 278px;
+  .word-box {
     margin: auto;
     font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
     text-transform: uppercase;
+  }
+
+  .letter-box {
+    position: relative;
+    text-align: center;
+  }
+
+  .letter-box:before {
+    content: '';
+    display: block;
+    width: 150px;
+    height: 150px;
+    background: #ff0783;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%) rotate(45deg);
+    border: 3px solid #222;
+    border-radius: 30%;
   }
 
   .current-word {
@@ -116,31 +134,40 @@
     width: 80px;
     height: 80px;
     border: 3px solid #222;
-    font-size: 22px;
+    font-size: 26px;
     font-weight: bold;
     text-align: center;
     line-height: 80px;
-    background-color: orange;
+    background-color: #ffc107;
     display: inline-block;
-    margin: 2px 0;
     position: relative;
+    border-radius: 100%;
+  }
+
+  .letter--1 {
+    top: -6px;
+  }
+
+  .letter--4 {
+    bottom: -6px;
   }
 
   .letter--0,
   .letter--2 {
-    bottom: -45px;
+    bottom: -34px;
   }
 
   .letter--3,
   .letter--5 {
-    bottom: 45px;
+    bottom: 34px;
   }
 
   .letter--main {
-    background-color: purple;
+    background-color: transparent;
+    border: none;
     color: #fff;
     display: block;
-    margin-left: 90px;
+    margin: auto;
   }
 
   .button-group, .valid-words {
@@ -174,17 +201,19 @@
   }
 </style>
 
-<div class="letter-box">
+<div class="word-box">
   <div class="validation-error">{validationError || ''}</div>
   <div class="current-word {validationError && 'current-word--error'}">{currentWord}</div>
-  {#each letters as letter, index}
-    <div on:click={handleLetterClick(letter)} class="letter letter--{index}">{letter}</div>
-    {#if index === 2}
-      <div on:click={handleLetterClick(mainLetter)} class="letter letter--main">{mainLetter}</div>
-    {/if}
-  {/each}
+  <div class="letter-box">
+    {#each letters as letter, index}
+      <div on:click={handleLetterClick(letter)} class="letter letter--{index}">{letter}</div>
+      {#if index === 2}
+        <div on:click={handleLetterClick(mainLetter)} class="letter letter--main">{mainLetter}</div>
+      {/if}
+    {/each}
+  </div>
   <div on:click={handleSubmit} class="button-group">
-    <button>submit</button>
+    <button>skicka</button>
   </div>
   <div class="valid-words">
     {#each validWords as word}
