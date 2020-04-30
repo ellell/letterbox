9b41fa2d52d7e5c7ca918a98293b2bc2177e832e
lisa-overmyr <lisa.overmyr@schibsted.com>
2020-04-30 17:04:23 +0200

    select letters and submit

--- src/app.svelte
+++ src/app.svelte
@@ -1,20 +1,51 @@
 <script>
+  let currentWord = '';
+  let validWords = [];
   const mainLetter = 'k';
-  const letters = ['a','l', 'b', 'o', 'm','s']
+  const letters = ['a','l', 'b', 'o', 'm','s'];
+
+  const handleLetterClick = (letter) => () => {
+    currentWord = currentWord + letter;
+  };
+
+  const isValidWord = (word) => {
+    return word.length > 2 && word.indexOf(mainLetter) !== -1;
+  }
+
+  const handleSubmit = () => {
+    if (isValidWord(currentWord)) {
+      validWords = validWords.concat([currentWord]);
+    }
+
+    currentWord = '';
+  };
 </script>
 
 <style>
   .letter-box {
     width: 278px;
     margin: auto;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    text-transform: uppercase;
+  }
+
+  .current-word {
+    height: 40px;
+    font-size: 30px;
+    text-align: center;
   }
 
   .letter {
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    cursor: pointer;
     width: 80px;
     height: 80px;
     border: 3px solid #222;
-    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
-    text-transform: uppercase;
     font-size: 22px;
     font-weight: bold;
     text-align: center;
@@ -41,13 +72,48 @@
     display: block;
     margin-left: 90px;
   }
+
+  .button-group, .valid-words {
+    text-align: center;
+    margin-top: 20px;
+  }
+
+  button {
+    border: 3px solid #222;
+    border-radius: 30px;
+    text-transform: uppercase;
+    cursor: pointer;
+    font-size: 22px;
+    outline: none;
+    padding: 10px 20px;
+    color: #222;
+    margin-left: -10px;
+  }
+
+  .valid-word::after {
+    content: "\00B7";
+    margin: 10px;
+  }
+
+  .valid-word:last-child::after {
+    content: '';
+  }
 </style>
 
 <div class="letter-box">
+  <div class="current-word">{currentWord}</div>
   {#each letters as letter, index}
-    <div class="letter letter--{index}">{letter}</div>
+    <div on:click={handleLetterClick(letter)} class="letter letter--{index}">{letter}</div>
     {#if index === 2}
-      <div class="letter letter--main">{mainLetter}</div>
+      <div on:click={handleLetterClick(mainLetter)} class="letter letter--main">{mainLetter}</div>
     {/if}
   {/each}
+  <div on:click={handleSubmit} class="button-group">
+    <button>submit</button>
+  </div>
+  <div class="valid-words">
+    {#each validWords as word}
+      <span class="valid-word">{word}</span>
+    {/each}
+  </div>
 </div>
